080ff19da5cb77167d20eaebda1801812cf2778e
ellenmariadev <ellenmariadev@gmail.com>
2022-12-21 22:41:58 -0300

    eslint, prettier, editorconfig settings

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "ignore",
+  endOfLine: "lf",
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+};
